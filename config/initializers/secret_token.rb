77538293cceea9616ff37b337f21e45f815e7f13
an1tahung <anitalhung@gmail.com>
2015-11-22 02:27:49 +0000

    Added untracked files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = 'afddd41bd86d89563b51ad488d5b0e1b5667d56ae25360a51f87a2cda196d61c86294c25cf6c0e4f85cafa345a8e779357e8a2ebca61899c0653b9aa67269263'
